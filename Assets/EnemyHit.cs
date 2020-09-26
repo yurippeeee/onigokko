d96b3bd88e62ae8dba29a585e1a1a2d18c839bd2
yurippeeee <isshi0929@icloud.com>
2020-09-26 10:03:40 +0900

    onigokko -version2-

--- Assets/EnemyHit.cs
+++ Assets/EnemyHit.cs
@@ -7,19 +7,20 @@ public class EnemyHit : MonoBehaviour
 {
     float damage = 0.34f;
     public bool attack_collision_flag = false;
+
     
-    public GameObject hpbar;
-    
+    //public GameObject hpbar;
+
     //オブジェクトと接触した瞬間に呼び出される
     void OnCollisionEnter(Collision other)
     {
         //攻撃した相手がEnemyの場合
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Player"))
         {
             if(attack_collision_flag == false)
             {
-                hpbar.GetComponent<Slider>().value -= damage;
-                if (hpbar.GetComponent<Slider>().value <= 0)
+                other.gameObject.transform.Find("HPUI").transform.Find("HPBar").GetComponent<Slider>().value -= damage;
+                if (other.gameObject.transform.Find("HPUI").transform.Find("HPBar").GetComponent<Slider>().value <= 0)
                 {
                     Destroy(other.gameObject);
                 }
